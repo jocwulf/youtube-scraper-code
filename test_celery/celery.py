26ef943dfe39fc6b32facafd9d4fea7a68c3aa12
Zeno Ziemke <ziemke@college.harvard.edu>
2016-07-19 02:28:54 +0200

    t

--- test_celery/celery.py
+++ test_celery/celery.py
@@ -11,7 +11,7 @@ BROKER_HEARTBEAT = None # We're using TCP keep-alive instead
 BROKER_CONNECTION_TIMEOUT = 30 # May require a long timeout due to Linux DNS timeouts etc
 CELERY_RESULT_BACKEND = None # AMQP is not recommended as result backend as it creates thousands of queues
 CELERY_SEND_EVENTS = False # Will not create celeryev.* queues
-CELERY_EVENT_QUEUE_EXPIRES = 60*5 # Will delete all celeryev. queues without consumers after 10 minutes.
+CELERY_EVENT_QUEUE_EXPIRES = 60*3 # Will delete all celeryev. queues without consumers after 3 minutes.
 
 # configuring best practices for using celery with bigwing hosted rabbitmq (http://www.lshift.net/blog/2015/04/30/making-celery-play-nice-with-rabbitmq-and-bigwig/)
 BROKER_TRANSPORT_OPTIONS = {'confirm_publish': True}
